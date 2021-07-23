12b924078434eeba7ab09ce8204b2d717b9d0331
KangHeeWon <kkhw1202@gmail.com>
2021-07-23 10:11:23 +0900

    Add to-do list delete btn controller init

---
+++ js/recycleBin.js
@@ -0,0 +1,6 @@
+let rBin = document.querySelector('.delList_btn');
+
+rBin.addEventListener('click', function () {
+    // 휴지통 열렸을때 아이콘 바뀌고 to do list 오른쪽에 delete 버튼 생성
+    // 휴지통 닫혀있을땐 닫힘 아이콘으로 바뀌고 delete 버튼 숨기기
+});
