63f8546a95983b67747731ffa07446fbf49c4a38
KangHeeWon <kkhw1202@gmail.com>
2021-09-05 20:36:13 +0900

    Implement show&hide in privacy information

--- js/gretting.js
+++ js/gretting.js
@@ -2,6 +2,9 @@ const form = document.querySelector('.js-form'),
     input = form.querySelector('input'),
     greeting = document.querySelector('.js-greetings');
 
+    let info = document.querySelector('.privacy_info');
+    const hide_toDoForm = document.querySelector('.js-toDoForm');
+
 const USER_LS = 'currentUser',
     SHOWING_CN = 'showing';
 
@@ -10,14 +13,17 @@ function saveName(text) {
 }
 
 function handleSubmit(event) {
+
     event.preventDefault();
     const currentValue = input.value;
     paintGreeting(currentValue);
     saveName(currentValue);
+    info.style.display = 'none';
 }
 
 function askForName() {
     form.classList.add(SHOWING_CN);
+    hide_toDoForm.style.display = 'none';
     form.addEventListener('submit', handleSubmit);
 }
 
@@ -25,6 +31,7 @@ function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
     greeting.innerText = `안녕하세요, ${text}님!`;
+    hide_toDoForm.style.display = 'block';
 }
 
 function loadName() {
@@ -36,6 +43,7 @@ function loadName() {
     } else {
         // she is
         paintGreeting(currentUser);
+        info.style.display = 'none';
     }
 
 }
