2e325c12163882cbf0da61f33991d8f9f8f8cec0
KangHeeWon <kkhw1202@gmail.com>
2021-09-19 18:59:32 +0900

    Make recycleBin toggle work

--- js/recycleBin.js
+++ js/recycleBin.js
@@ -1,6 +1,22 @@
-let rBin = document.querySelector('.delList_btn');
+let rBin = document.querySelector('.recycleBin_btn'),
+    rBinImg = rBin.querySelector('img');
+let controlBin = document.querySelectorAll('button.del_btn');
 
 rBin.addEventListener('click', function () {
     // 휴지통 열렸을때 아이콘 바뀌고 to do list 오른쪽에 delete 버튼 생성
     // 휴지통 닫혀있을땐 닫힘 아이콘으로 바뀌고 delete 버튼 숨기기
+    if(rBinImg.getAttribute('src') === '../img/icon_recycleBin_close.svg') {
+    // 닫혀있을때 클릭하면
+        rBinImg.src = '../img/icon_recycleBin_open.svg';
+        for(let i=0; i<controlBin.length; i++) {
+            controlBin[i].style.visibility = 'visible';
+        }
+    }
+    else {
+    // 열려있을때 클릭하면
+        rBinImg.src = '../img/icon_recycleBin_close.svg';
+        for(let i=0; i<controlBin.length; i++) {
+            controlBin[i].style.visibility = 'hidden';
+        }
+    }
 });
